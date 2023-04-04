1138de21c6b3f27dc7ff3c7a019942dadd55aabd
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-04-04 08:06:24 +0300

    connect to database

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://yazeedmomani:Marius%402021@free-cluster.bx6mu6s.mongodb.net/body-metrics?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database connection successful."))
+  .catch((error) => console.log("Database connection failed", error));
 
 const PORT = process.env.PORT || 3000;
 
